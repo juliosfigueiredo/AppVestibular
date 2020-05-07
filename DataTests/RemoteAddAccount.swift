a2c8ec06dc338ecdc817942f97a43d79848ca168
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-07 20:44:15 -0300

    Adicionado RemoteAddAccount testes.

---
+++ DataTests/RemoteAddAccount.swift
@@ -0,0 +1,16 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by Julio Figueiredo on 07/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import XCTest
+
+class RemoteAddAccount: XCTestCase {
+
+    func test_() {
+        
+    }
+}
