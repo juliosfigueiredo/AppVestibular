d3f243ffa7a2a05b852e6de05fe1bba8bafc3e65
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-08 22:07:38 -0300

    Adicionado Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,19 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Julio Figueiredo on 08/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
