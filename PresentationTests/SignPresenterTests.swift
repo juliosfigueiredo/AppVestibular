073be768eba67a8bf740a0800537b85e94877f74
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-10 02:06:33 -0300

    Adicionado Presentation layer

---
+++ PresentationTests/SignPresenterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Julio Figueiredo on 10/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import XCTest
+@testable import Presentation
+
+class SignPresenterTests: XCTestCase {
+    func test_() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
