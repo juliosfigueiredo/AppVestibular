9be50686bd68f8ab43921e85b310b4e3dd647490
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-12 21:38:31 -0300

    Permitir que todos targets rodem no iOS e macOS

---
+++ AppVestibularTests/SignUpViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppVestibularTests.swift
+//  AppVestibularTests
+//
+//  Created by Julio Figueiredo on 06/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+        
+    }
+}
