265f020ccf2f6a334780a8c3565eb5be30ff49be
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-14 21:41:37 -0300

    Adicionado Core Scheme

---
+++ Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Julio Figueiredo on 14/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
