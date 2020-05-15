24fedbc0c6c89e8e08b4192d7795e25b709ea7b9
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-14 21:16:41 -0300

    Adicionado arquivos das controllers de Sign e AddAccount

--- AppVestibular/Controllers/AddAccountViewController.swift
+++ AppVestibular/Controllers/AddAccountViewController.swift
@@ -1,24 +1,29 @@
 //
-//  SignUpViewController.swift
+//  AddAccountViewController.swift
 //  AppVestibular
 //
-//  Created by Julio Figueiredo on 12/05/20.
+//  Created by Julio Figueiredo on 14/05/20.
 //  Copyright © 2020 Julio Figueiredo. All rights reserved.
 //
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class AddAccountViewController: UIViewController {
     @IBOutlet weak var tfCpf: UITextField!
+    @IBOutlet weak var tfNome: UITextField!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfDataNascimento: UITextField!
+    @IBOutlet weak var tfAnoConclusao: UITextField!
     @IBOutlet weak var tfSenha: UITextField!
-    @IBOutlet weak var btEntrar: UIButton!
+    @IBOutlet weak var tfConfirmarSenha: UITextField!
     @IBOutlet weak var btCadastrar: UIButton!
     @IBOutlet weak var loading: UIActivityIndicatorView!
     @IBOutlet weak var vwLoading: UIView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
+        
     }
 }
